8cd4252c6c35f9d5fd24837aee4e4bda11f8986a
BaiSeDeXiaoHongHua <67800300+HaoHaoShuoHu-a@users.noreply.github.com>
2020-07-11 20:59:47 +0800

    Update 1_practice_1.c

--- 1_practice_1.c
+++ 1_practice_1.c
@@ -3,17 +3,8 @@ int main(void)
 {
     int x;
     printf("Please enter a length in English units and this program will convert it to metric units: (0 to quit)\n");
-    while (scanf("%d", &x) && x != 0)
-    {
-        if (x < 0)
-            printf("Error: Wrong input!\n");
-        else if (x == 1)
-            printf("An inch is equal to 2.54 centimeters.\n");
-        else
-            printf("%d inches are equal to %.2f centimeters.\n", x, x * 2.54);
-        printf("Please enter a length in English units and this program will convert it to metric units: (0 to quit)\n");
-    }
-    printf("Program determination.");
+    scanf("%d", &x);
+    printf("%d inches are equal to %.2f centimeters.\n", x, x * 2.54);
 
     return 0;
 }
